20b09e2d219c0fd0554c043c7603ce37b8c03fc7
Гирский Андрей <andreygirskiy291@gmail.com>
2025-06-27 15:52:34 +0500

    final try

--- app/src/infrastructure/db/test_settings.py
+++ app/src/infrastructure/db/test_settings.py
@@ -3,19 +3,19 @@ import os
 from pathlib import Path
 
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: str
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    DB_TEST_HOST: str
+    DB_TEST_PORT: str
+    DB_TEST_USER: str
+    DB_TEST_PASS: str
+    DB_TEST_NAME: str
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_TEST_USER}:{self.DB_TEST_PASS}@{self.DB_TEST_HOST}:{self.DB_TEST_PORT}/{self.DB_TEST_NAME}"
     
     class Config:
         base_dir = Path(__file__).resolve().parent.parent.parent.parent
-        env_file = os.path.join(base_dir, ".test.env")
+        env_file = os.path.join(base_dir, ".env")
         env_file_encoding = "utf-8"
         extra = 'ignore'
 
